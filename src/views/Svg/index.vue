8ab85705ff7adeed60d54bbcd3f30f5646e5235c
changecoder <changecoder@outlook.com>
2022-08-29 23:42:42 +0800

    HTML与SVG的结合

--- src/views/Svg/index.vue
+++ src/views/Svg/index.vue
@@ -1,65 +1,113 @@
 <template>
   <div class="data-view">
-    <h1>矩形</h1>
-    <svg>
-      <rect
-        x="50"
-        y="20"
-        width="150"
-        height="150"
-        rx="20"
-        ry="20"
-        style="
-          fill:blue;
-          stroke:pink;
-          stroke-width:5;
-          fill-opacity:0.1;
-          stroke-opacity:0.9
-        "/>
-    </svg>
-    <h1>文本</h1>
-    <svg>
-      <text x="10" y="20" style="fill:red;">Several lines:
-        <tspan x="10" y="45" width="20px">First line</tspan>
-        <tspan x="10" y="70">Second line</tspan>
-      </text>
-    </svg>
-    <h1>自定义路径</h1>
-    <svg>
-      <defs>
-        <path id="path1" d="M0,0 L0,80 M30,0 L30,100" />
-      </defs>
-      <text x="10" y="100"  style="fill:red; writing-mode: tb;">
-        <textPath xlink:href="#path1">我的名字叫查理的爸爸</textPath>
-      </text>
-    </svg>
-    <h1>标记</h1>
-    <svg width="500" height="100">
-      <defs>
-        <marker id="markerCircle" markerwidth="8" markerheight="8" refx="5" refy="5">
-          <circle cx="5" cy="5" r="3" style="stroke: none; fill:#000000;"></circle>
-        </marker>
-        <marker id="markerArrow" markerwidth="13" markerheight="13" refx="2" refy="6" orient="auto">
-          <path d="M2,2 L2,11 L10,6 L2,2" style="fill: #000000;"></path>
-        </marker>
-      </defs>
-      <path d="M100,10 L150,10 L150,60" style="stroke: #6666ff; stroke-width: 1px; fill: none;
-        marker-start: url(#markerCircle);
-        marker-end: url(#markerArrow);
-      "></path>
-    </svg>
-    <h1>竖排显示文字</h1>
-    <div style="writing-mode:vertical-lr;max-height: 50px;">我是文字我是文字我是文字我是蚊子</div>
+    <h1>HTML与SVG的结合</h1>
+    <div class="container">
+      <svg class="svg-container" style="transform: scale(1) translate(0px, 0px); transform-origin: center center;">
+      </svg>
+      <div class="dom-container" style="transform: scale(1) translate(0px, 0px); transform-origin: center center;">
+        <div
+          class="node"
+          style="left: 100px; top: 100px; width: 100px; height: 40px;"
+        >测试一</div>
+        <div
+          class="node"
+          style="left: 250px; top: 50px; width: 100px; height: 40px;"
+        >测试二</div>
+        <div
+          class="node"
+          style="left: 250px; top: 150px; width: 100px; height: 40px;"
+        >测试三</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import * as d3 from 'd3'
+
 export default {
-  name: 'SvgPage'
+  name: 'SvgPage',
+  data () {
+    return {
+      svgSelect: {},
+      dx: 30,
+      dy: 20,
+      markerId: 'markerArrow'
+    }
+  },
+  mounted () {
+    this.svgSelect = d3.select('svg')
+    this.drawMarker()
+    this.drawPath({ x: 150, y: 100 }, { x: 200, y: 150 })
+    this.drawPath({ x: 150, y: 100 }, { x: 200, y: 50 })
+  },
+  methods: {
+    drawMarker () {
+      this.svgSelect
+        .append('defs')
+        .append('marker')
+        .attr('id', this.markerId)
+        .attr('viewBox', '0 -5 10 10')
+        .attr('refX', 15)
+        .attr('refY', -1.5)
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 6)
+        .attr('orient', 'auto')
+    },
+    drawPath (source, target) {
+      const { x: sourceX, y: sourceY } = source
+      const { x: targetX, y: targetY } = target
+      const { dx, dy } = this
+      const path = d3.path()
+      const cpx1 = sourceX + dx
+      const cpy1 = sourceY - dy
+      const cpx2 = targetX - dx
+      const cpy2 = targetY + dy
+      path.moveTo(sourceX, sourceY)
+      path.bezierCurveTo(cpx1, cpy1, cpx2, cpy2, targetX, targetY)
+      this.svgSelect.append('path')
+        .attr('d', path.toString())
+        .attr('marker-end', `url(#${this.markerId})`)
+        .style('fill', 'none')
+        .style('stroke', 'red')
+        .style('stroke-width', '2')
+    }
+  }
 }
 </script>
-<style>
+<style lang="less">
   .data-view {
     height: 100vh;
+  }
+  .container {
+    position: relative;
+    width: 400px;
+    height: 400px;
+    border: 1px solid grey;
+
+    .svg-container,
+    .dom-container {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      left: 0;
+      top: 0;
+    }
+
+    .dom-container {
+      position: absolute;
+
+      .node {
+        cursor: pointer;
+        position: absolute;
+        border: 1px solid #E2E2E2;
+        border-radius: 25%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        transition: all 0.2s;
+      }
+    }
   }
 </style>
